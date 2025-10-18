d9f061dff0f0444148c7122b2d7f750fbcc82ced
mangesh687 <mangeshpakhale2016@gmail.com>
2025-10-18 16:43:43 +0530

    without using third varibale

---
+++ impcodingmustread/SwipeTwoNumberwithoutThirdVaribale.java
@@ -0,0 +1,18 @@
+package impcodingmustread;
+
+public class SwipeTwoNumberwithoutThirdVaribale {
+	public static void main(String[] args) {
+ //Q #2) Write a Java Program to swap two numbers without using the third variable.
+		int a=10;
+		int b=20;
+		int tem;
+		
+		a=a+b;//30
+		b=a-b;//10
+		a=a-b;//2o
+		
+		System.out.println(a+" : "+b);
+
+	}
+
+}
