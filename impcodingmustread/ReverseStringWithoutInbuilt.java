5d039af4916ca6494cec6e144ecbf6e2528abdd5
mangesh687 <mangeshpakhale2016@gmail.com>
2025-10-18 16:12:22 +0530

    revese string

--- impcodingmustread/ReverseStringWithoutInbuilt.java
+++ impcodingmustread/ReverseStringWithoutInbuilt.java
@@ -3,8 +3,18 @@ package impcodingmustread;
 public class ReverseStringWithoutInbuilt {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String str="java";
+		String rev="";
+for(int i=str.length()-1;i>=0;i--) {
+	rev=rev+str.charAt(i);
+}
+System.out.println(rev);
+System.out.println(str);
+if(str.equals(rev)) {
+	System.out.println("string is palindarm");
+}else {
+	System.out.println("string not a palindarm");
+}
 	}
 
 }
