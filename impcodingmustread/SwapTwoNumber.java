df8826bba99ea4ef4134833e4cb705d8efdab001
mangesh687 <mangeshpakhale2016@gmail.com>
2025-10-18 16:45:54 +0530

    using third varibale swap number

---
+++ impcodingmustread/SwapTwoNumber.java
@@ -0,0 +1,13 @@
+package impcodingmustread;
+
+public class SwapTwoNumber {
+	public static void main(String[] args) {
+		int a=10,b=20;
+		int tem;
+		tem=a;
+		a=b;
+		b=tem;
+		System.out.println(a+" "+b);
+	}
+
+}
