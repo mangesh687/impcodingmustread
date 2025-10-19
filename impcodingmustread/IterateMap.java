39902d1129ef560d0dda70ece23744c29decc60f
mangesh687 <mangeshpakhale2016@gmail.com>
2025-10-19 10:17:23 +0530

    IterateMap

--- impcodingmustread/IterateMap.java
+++ impcodingmustread/IterateMap.java
@@ -6,18 +6,18 @@ import java.util.Map.Entry;
 
 public class IterateMap {
 	public static void main(String[] args) {
-		//Write a Java Program to iterate HashMap using While and advance for loop
-		
-		Map<String, String> data=new HashMap<>();
-		 data.put("A", "Apple");
-	     data.put("B", "Ball");
-	     data.put("C", "Cat");
-	     data.put("D", "Dog");
-	     data.put("E", "Elephant");
-	     for(Map.Entry<String, String> sc: data.entrySet()) {
-	    	 System.out.println(sc.getKey()+" : "+sc.getValue());
-	     }
+		// Write a Java Program to iterate HashMap using While and advance for loop
+
+		Map<String, String> data = new HashMap<>();
+		data.put("A", "Apple");
+		data.put("B", "Ball");
+		data.put("C", "Cat");
+		data.put("D", "Dog");
+		data.put("E", "Elephant");
+		for (Map.Entry<String, String> sc : data.entrySet()) {
+			System.out.println(sc.getKey() + " : " + sc.getValue());
+		}
 
 	}
-	
+
 }
