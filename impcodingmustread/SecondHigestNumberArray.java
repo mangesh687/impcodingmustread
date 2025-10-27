261b9074ca191e7eaba1f4bc7341569c6bf81bba
mangesh687 <mangeshpakhale2016@gmail.com>
2025-10-27 12:46:13 +0530

    second highest number

--- impcodingmustread/SecondHigestNumberArray.java
+++ impcodingmustread/SecondHigestNumberArray.java
@@ -6,10 +6,26 @@ import java.util.stream.Collectors;
 
 public class SecondHigestNumberArray {
 	public static void main(String[] args) {
-		int[] a= {4,10,-5,-7,10,11,22};
+		int[] a= {4,10,-5,-7,7,10,11,22};
+		//Write a Java Program to find the second-highest number in an array.
+//		Arrays.sort(a);
+//		System.out.println(a[a.length-2]);
 		
-		Arrays.sort(a);
-		System.out.println(a[a.length-2]);
+		int highest=0;
+		int secondhighest=0;
+		for(int num:a) {
+			if(num>highest) {
+				secondhighest=highest;
+				highest=num;
+			}
+			else if (num>secondhighest && num!=highest) {
+				secondhighest=num;
+			}
+		}
+		
+		System.out.println(secondhighest);
+		
+		
 	}
 
 }
